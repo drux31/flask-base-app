983c50a14c37e65649d3b69b0259f02226ca316c
drux <landry@lnts.me>
2025-12-01 17:07:50 +0100

    init of application and database

---
+++ trainr/schema.sql
@@ -0,0 +1,19 @@
+drop table if exists user;
+drop table if exists post;
+
+create table user(
+
+    user_id integer primary key AUTOINCREMENT,
+    username text unique not null,
+    password text not null
+);
+
+
+create table post (
+    post_id integer primary key AUTOINCREMENT,
+    author_id integer not null,
+    created timestamp not null default current_timestamp,
+    title text not null,
+    body text not null,
+    foreign key (author_id) references user(user_id)
+);
